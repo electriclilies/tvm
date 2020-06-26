d0b36568dba13b3e06ad3205e8c1fdf23797cf87
Lily Orth-Smith <lorthsmith@Lilys-MacBook-Pro.local>
2020-06-25 18:00:13 -0700

    removed some dyn stuff from broadcast_to

--- src/relay/op/tensor/transform.cc
+++ src/relay/op/tensor/transform.cc
@@ -1718,25 +1718,22 @@ bool BroadCastToRel(const Array<Type>& types, int num_inputs, const Attrs& attrs
                     const TypeReporter& reporter) {
   CHECK_EQ(types.size(), 3);
   const InitOpAttrs* param = attrs.as<InitOpAttrs>();
-  const auto* target_shape = types[1].as<TensorTypeNode>();
-  DataType out_dtype = types[0].as<TensorTypeNode>()->dtype;
+  const auto* target_shape = types[1].as<TensorTypeNode>(); //type of target shape
+  DataType out_dtype = types[0].as<TensorTypeNode>()->dtype; // output type same as input type
 
-  const IntImmNode* shape_shape = target_shape->shape[0].as<IntImmNode>();
-  CHECK(shape_shape) << "Parameter shape must have static shape";
+
+  const IntImmNode* rank = target_shape->shape[0].as<IntImmNode>();
+  CHECK(rank) << "Parameter shape must have static rank"; // shape_shape = rank, rank must be static (even in dyn pass)
 
   std::vector<IndexExpr> oshape;
-  if (param->shape) {
-    const Array<Integer>& cshape_array = param->shape.value();
+
+  const Array<Integer>& cshape_array = param->shape.value(); // static case
     for (size_t i = 0; i < cshape_array.size(); ++i) {
-      oshape.push_back(cshape_array[i]);
-    }
-  } else {
-    for (int i = 0; i < shape_shape->value; ++i) {
-      oshape.push_back(Any());
-    }
+      oshape.push_back(cshape_array[i]); // put in output shape as compile time const 
   }
-  reporter->Assign(types[2], TensorType(oshape, out_dtype));
-  return BroadcastRel({types[0], types[2], types[2]}, 2, Attrs(), reporter);
+  reporter->Assign(types[2], TensorType(oshape, out_dtype)); // assigns output to infered shape
+  return BroadcastRel({types[0], types[2], types[2]}, 2, Attrs(), reporter); // don't do this bc check
+
 }
 
 Expr MakeBroadCastTo(Expr data, Expr shape) {

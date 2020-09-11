ae5f8b1e264238b06330deb3d41f6534bff06b7a
electriclilies <lilyorthsmith@gmail.com>
2020-09-10 17:22:04 -0700

    test

--- python/tvm/shape_dsl/test.py
+++ python/tvm/shape_dsl/test.py
@@ -1,4 +1,5 @@
 import tvm
+from tvm import te
 from typing import List
 
 class ShapeExpr(object):
@@ -16,6 +17,16 @@ class ShapeTuple(ShapeExpr):
 my_tuple = ShapeTuple([1, 2, 3, 4])
 print(my_tuple.dims)
 
+def arange_shape_func():
+    start = te.var("start")
+    stop = te.var("stop")
+    step = te.var("step")
+    inputs = [start, stop, step]
+    out = tvm.tir.Cast("int64", tvm.tir.ceil(tvm.tir.Div(tvm.tir.Sub(start, stop), step)))
+    # this causes an error
+    func = tvm.tir.PrimFunc(inputs, out)
+    mod = tvm.ir.IRModule(functions=[func])
+    
 
 @tvm.register_func("transpose_shape")
 def handwritten_arange_func(start, stop, step):
@@ -23,6 +34,6 @@ def handwritten_arange_func(start, stop, step):
     rank = int((stop - start) / step)
     return ShapeTuple(rank) # how do I register ShapeTuple with FFI_API and what does register_func actually do
 
-print(handwritten_arange_func(1, 3, 1).dims)
+arange_shape_func()
+#print(handwritten_arange_func(1, 3, 1).dims)
 
-def arange_shape_func()

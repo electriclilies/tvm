24b7fa18496627dd480c877ce354aadcd8f2333e
electriclilies <lilyorthsmith@gmail.com>
2020-09-21 13:17:31 -0700

    more updates

--- python/tvm/relay/new_quantize/quantize_pass.py
+++ python/tvm/relay/new_quantize/quantize_pass.py
@@ -5,9 +5,9 @@ from tvm.relay.frontend.common import infer_type
 
 def quantize(func):
     class QuantizeMutator(ExprMutator):
+        # add pad explicitly
         def visit_call(self, call):
             if call.op == relay.op.get('nn.conv2d'):
-                #TODO Actually use quantize instead of hard casting
                 zero_point = relay.const(0, dtype='int32')
                 scale = relay.const(1, dtype='float32')
                 args = [relay.qnn.op.quantize(self.visit(arg), scale, zero_point) for arg in call.args]
@@ -20,8 +20,12 @@ def quantize(func):
                     if isinstance(attr_value, tvm.ir.container.Array):
                         attr_value = tuple(attr_value)
                     if attr == 'kernel_size':
-                        #TODO Add handlng for None
-                        kernel_size = tuple([k.value for k in call.attrs[attr]])
+                        kernel_size = call.attrs[attr]
+                        if kernel_size is None:
+                            # what to do?
+                            type_info = infer_type()
+                        else:
+                            kernel_size = tuple([k.value for k in call.attrs[attr]])
                     elif attr == 'channels':
                         channels = call.attrs[attr]
                         if channels is None:
@@ -35,7 +39,6 @@ def quantize(func):
                 # Override output dtype.
                 new_attr_dict['out_dtype'] = 'int32'
                 qnn_call = relay.qnn.op.conv2d(*args, **new_attr_dict)
-                #TODO We should dequantize instead of cast.
                 return relay.qnn.op.dequantize(qnn_call, scale, zero_point)
             else:
                 return super().visit_call(call)
--- python/tvm/relay/new_quantize/test.py
+++ python/tvm/relay/new_quantize/test.py
@@ -1,11 +1,30 @@
 import tvm
 import onnx
+import torch
+from torchvision.models import resnet
 from tvm import relay
-from quantize_pass import quantize
+import quantize_pass
 
+# ONNX TEST
 onnx_model = onnx.load('resnet18-v1-7.onnx')
 input_dict = {'data': [1, 3, 224, 224]}
 mod, params = relay.frontend.from_onnx(onnx_model, input_dict)
-mod = quantize(mod['main'])
+mod = quantize_pass.quantize(mod['main'])
 
+print("ONNX resnet 18 quantized with our quantize pass:")
+print(mod['main'])
+
+
+pytorch_model = resnet.resnet18(pretrained=True)
+input_name = "input"  # the input name can be be arbitrary for PyTorch frontend.
+input_shape = (1, 3, 224, 224)
+named_input_shape = [(input_name, input_shape)]
+input_data = torch.randn(input_shape)
+script_module = torch.jit.trace(pytorch_model, input_data)
+
+input_shapes = [(input_name, (1, 3, 224, 224))]
+mod, params = relay.frontend.from_pytorch(script_module, named_input_shape)
+mod = quantize_pass.quantize(mod['main'])
+
+print("Pytorch resnet 18 quantized with our quantize pass:")
 print(mod['main'])
